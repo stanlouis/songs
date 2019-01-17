e91e9d7361ac6d9d08f59faddaae518342e7a513
Stanley Louis <stanlouis@msn.com>
2019-01-17 06:57:35 -0500

    Building SongList.js with data from redux

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import SongList from './SongList';
 
 const App = () => (
-  <div>
-    <SongList />
+  <div className="ui container grid">
+    <div className="ui row">
+      <div className="column eight wide">
+        <SongList />
+      </div>
+    </div>
   </div>
 );
 
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class SongList extends Component {
+  renderList() {
+    const { songs } = this.props;
+    return songs.map(song => (
+      <div className="item" key={song.title}>
+        <div className="right floated content">
+          <button type="button" className="ui button primary">
+            Select
+          </button>
+        </div>
+        <div className="content">{song.title}</div>
+      </div>
+    ));
+  }
+
   render() {
     // console.log(this.props)
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -14,3 +29,7 @@ const mapStateToProps = state => ({ songs: state.songs });
 
 // provide the piece of state needed as props to this component
 export default connect(mapStateToProps)(SongList);
+
+SongList.propTypes = {
+  songs: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
